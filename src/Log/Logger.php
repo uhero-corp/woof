5b8289b61750071640649debf11fe3f124328a1d
KITO, takayuki <takayuki.kito@uhero.co.jp>
2019-11-12 13:56:57 +0900

    Add: LogFormat, DefaultLogFormat

---
+++ src/Log/Logger.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Woof\Log;
+
+class Logger
+{
+    /**
+     * @var int
+     */
+    const LEVEL_ERROR = 0;
+
+    /**
+     * @var int
+     */
+    const LEVEL_ALERT = 1;
+
+    /**
+     * @var int
+     */
+    const LEVEL_INFO  = 2;
+
+    /**
+     * @var int
+     */
+    const LEVEL_DEBUG = 3;
+}
