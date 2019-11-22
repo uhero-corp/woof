2d3b0dd85aa3cc29b3d8a2a56bd564a5312bb1a9
KITO, takayuki <takayuki.kito@uhero.co.jp>
2019-11-22 14:35:35 +0900

    Add: SessionContainer, FileSessionContainer

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -46,4 +46,39 @@ class TestHelper
         }
         return $result;
     }
+
+    /**
+     * 指定されたディレクトリのファイルすべてを、もう一方のディレクトリにコピーします。
+     * サブディレクトリが存在していた場合は再帰的にコピーされます。
+     * ただし .gitignore や .gitkeep など "." から始まるファイル名のものは除外されます。
+     *
+     * @param string $sourcedir
+     * @param string $targetdir
+     * @return bool
+     */
+    public static function copyDirectory(string $sourcedir, string $targetdir): bool
+    {
+        $result = self::mkdirAll($targetdir);
+        $di     = new DirectoryIterator($sourcedir);
+        foreach ($di as $i) {
+            // "." や ".." などのディレクトリと, その他 '.' から始まるファイル名を除外します
+            $filename = $i->getFilename();
+            if (substr($filename, 0, 1) === ".") {
+                continue;
+            }
+            $sourcePath = "{$sourcedir}/{$filename}";
+            $targetPath = "{$targetdir}/{$filename}";
+            $result     = $result && ($i->isDir() ? self::copyDirectory($sourcePath, $targetPath) : copy($sourcePath, $targetPath));
+        }
+        return $result;
+    }
+
+    /**
+     * @param string $dirname
+     * @return bool
+     */
+    private static function mkdirAll(string $dirname): bool
+    {
+        return is_dir($dirname) || (self::mkdirAll(dirname($dirname)) && mkdir($dirname) && chmod($dirname, 0777));
+    }
 }
